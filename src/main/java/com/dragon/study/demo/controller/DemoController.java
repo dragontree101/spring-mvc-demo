fb43aa028c54121db68f689ded80cd2dffd01ecd
longzhe <dragonlong1986@126.com>
2016-02-25 09:20:46 +0800

    加入了config模块读取配置文件

--- src/main/java/com/dragon/study/demo/controller/DemoController.java
+++ src/main/java/com/dragon/study/demo/controller/DemoController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.dragon.study.demo.constant.Constant;
 import com.dragon.study.demo.service.IDemoService;
 import com.dragon.study.demo.vo.Message;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,4 +44,22 @@ public class DemoController {
         Message msg = new Message(Constant.STATUS_OK, "ok");
         return JSON.toJSONString(msg);
     }
+
+    @RequestMapping(value = "visitConfigDemo", produces = Constant.CONTENT_TYPE)
+    @ResponseBody
+    public String visitConfigDemo(HttpServletRequest request) {
+        logger.info("start visit config demo");
+        Config appConfig1 = ConfigFactory.load("app-config-1.conf");
+        logger.info("config1, complex-app.something=" + appConfig1.getString("complex-app.something"));
+        logger.info("config1, simple-lib.foo=" + appConfig1.getString("simple-lib.foo"));
+        logger.info("config1, simple-lib.whatever=" + appConfig1.getString("simple-lib.whatever"));
+
+        Config appConfig2 = ConfigFactory.load("app-config-2.conf");
+        logger.info("config2, complex-app.simple-lib-context.simple-lib.foo=" + appConfig2.getString("complex-app.simple-lib-context.simple-lib.foo"));
+        logger.info("config2, complex-app.simple-lib-context.simple-lib.whatever=" + appConfig2.getString("complex-app.simple-lib-context.simple-lib.whatever"));
+
+        Message msg = new Message(Constant.STATUS_OK, "ok");
+        return JSON.toJSONString(msg);
+    }
+
 }
